7839cfee760b025ca0c313cbe264ac03cd7c6594
Nguyen Le Dai Phi <nguyenledaiphi0252005@gmail.com>
2025-10-14 16:50:16 +0700

    Create login part for doctor

---
+++ web/src/main/java/com/e_health_care/web/doctor/controller/Login.java
@@ -0,0 +1,15 @@
+package com.e_health_care.web.doctor.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.e_health_care.web.doctor.dto.DoctorDTO;
+
+@RestController
+public class Login {
+    // @PostMapping("/doctor/signin")
+    // public String signIn(@RequestBody DoctorDTO doctorDTO) {
+        
+    // }
+}
