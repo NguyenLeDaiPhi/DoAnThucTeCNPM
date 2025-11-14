3c32bdfa58766377d10e8e7724069eb29aeddd25
Nguyen Le Dai Phi <nguyenledaiphi0252005@gmail.com>
2025-11-14 16:31:56 +0700

    Update clinical infor for patient

---
+++ web/src/main/java/com/e_health_care/web/patient/dto/PatientDTO.java
@@ -0,0 +1,20 @@
+package com.e_health_care.web.patient.dto;
+
+import lombok.Data;
+
+@Data
+public class PatientDTO {
+    private long id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private int phone;
+
+    private String role = "patient";
+
+    public String getUppercase_role() {
+        return this.role.toUpperCase();
+    }
+}
