8f3aa1c7e7b45951dcfc2ed9a9b185651f9b25a6
Dương Lê Quốc Khánh <khanh0328873909@gmail.com>
2025-11-15 18:51:44 +0700

    update GUI patient update

---
+++ web (1)/src/main/java/com/e_health_care/web/patient/service/PatientService.java
@@ -0,0 +1,45 @@
+package com.e_health_care.web.patient.service;
+
+import com.e_health_care.web.patient.dto.PatientDTO;
+import com.e_health_care.web.patient.model.Patient;
+import com.e_health_care.web.patient.repository.PatientRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientService {
+    @Autowired
+    private PatientRepository patientRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public PatientDTO getPatientById(long id) {
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Not found patient"));
+
+        PatientDTO patientDto = new PatientDTO();
+        BeanUtils.copyProperties(patient, patientDto);
+        return patientDto;
+    }
+
+    public void updatePatient(PatientDTO dto, long id) {
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Not found patient"));
+
+        patient.setFirstName(dto.getFirstName());
+        patient.setLastName(dto.getLastName());
+        patient.setAddress(dto.getAddress());
+        patient.setPhone(dto.getPhone());
+        patient.setEmail(dto.getEmail());
+        patient.setPassword(dto.getPassword());
+
+        if (dto.getPassword() != null && !dto.getPassword().isEmpty()) {
+            patient.setPassword(passwordEncoder.encode(dto.getPassword()));
+        }
+
+        patientRepository.save(patient);
+    }
+
+}
